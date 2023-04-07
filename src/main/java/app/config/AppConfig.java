745eabf8f0c92ed1d950b35d6f9f98cae74e4dbe
Anna <ausviridova@gmail.com>
2023-04-07 17:20:04 +0400

    Созданы бины Dog и Timer, связаны с AnimalsCage для успешной проверки главного и тестовых методов.

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,25 @@
 package app.config;
 
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean(name = "dog")
+    public Dog getHelloWorld() {
+        Dog dog1 = new Dog();
+        dog1.setName("Шарик");
+        return dog1;
+    }
+    @Bean(name = "timer")
+    public Long getTime() {
+        Timer timer = new Timer();
+        return timer.getTime();
+    }
 }
---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,20 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal {
+    String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+}
